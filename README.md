3d962c8beca05964c9cb63f3f9c175407570dd47
Даниил <crotenhics@gmail.com>
2017-01-22 21:59:02 +0300

    Первый коммит епта

---
+++ README.md
@@ -0,0 +1,15 @@
+# mendeleev-ebal
+
+Таблица Менделеева ебущая тебя в рот
+
+## how to instal
+============================================================
+
+Сначала нужно сделать git clone
+> git clone https://github.com/hackwork-tymm0/mendeleev-ebal.git
+
+Переходим в папку с проектом
+> cd %PATH_TO_PROJECT%
+
+И запускаем
+> node main.js
---
+++ main.js
@@ -0,0 +1,22 @@
+var bracket = ")";
+setTimeout(function () {
+    console.log("н-водород");
+}, 2000);
+setTimeout(function () {
+    console.log("c-углерод");
+}, 4000);
+setTimeout(function () {
+    console.log("о-кислород");
+}, 6000);
+setTimeout(function () {
+    console.log("я");
+}, 8000);
+setTimeout(function () {
+    console.log("ебал твой рот");
+}, 10000);
+setTimeout(function () {
+    setInterval(function () {
+        console.log(bracket);
+        bracket += ")";
+    }, 1000);
+}, 12000);
